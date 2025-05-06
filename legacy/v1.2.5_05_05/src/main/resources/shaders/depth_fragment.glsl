caefea7ae3a007f5df0205033047c6c6c245edb3
Tobiasz Grala <90502595+Pjongi@users.noreply.github.com>
2025-05-06 18:33:53 +0200

    Poprawiona struktura plików

--- legacy/v1.2.5_05_05/src/main/resources/shaders/depth_fragment.glsl
+++
@@ -1,7 +0,0 @@
-#version 330 core
-// Minimalny fragment shader dla depth pass
-// Nic nie musi robić, głębia jest zapisywana automatycznie
-void main()
-{
-    // gl_FragDepth = gl_FragCoord.z; // Zwykle niepotrzebne, jeśli nie modyfikujesz głębi
-}
